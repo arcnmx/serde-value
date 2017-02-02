984ef4529aed5697b2485f3340a3fae216ea04b4
arcnmx <arcnmx@users.noreply.github.com>
2017-02-02 13:07:00 -0500

    smoke test

--- src/lib.rs
+++ src/lib.rs
@@ -561,3 +561,24 @@ impl de::Deserializer for Value {
                 deserialize_ignored_any();
             }
 }
+
+#[test]
+fn smoke_test() {
+	// some convoluted Value
+	let value = Value::Option(Some(Box::new(Value::Seq(vec![
+		Value::U16(8),
+		Value::Char('a'),
+		Value::F32(1.0),
+		Value::String("hello".into()),
+		Value::Map(vec![
+			(Value::Bool(false), Value::Unit),
+			(Value::Bool(true), Value::Newtype(Box::new(
+				Value::Bytes(b"hi".as_ref().into())
+			))),
+		].into_iter().collect()),
+	]))));
+
+	// assert that the value remains unchanged through {de,}serialization
+	let value_de = Value::deserialize(value.clone()).unwrap();
+	assert_eq!(value_de, value);
+}
