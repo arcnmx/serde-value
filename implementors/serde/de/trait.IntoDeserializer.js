cb5c2b3296eac399e5d42e81ddcd82523f759fc3
ghost <ghost@konpa.ku>
2017-04-25 18:06:53 +0000

    0.5.0 - 0d0a21b17cb9c739d888bde4c8ac96bf2ab8d1fa

---
+++ implementors/serde/de/trait.IntoDeserializer.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["serde_value"] = ["impl&lt;'de&gt; <a class='trait' href='https://docs.rs/serde/1.0.1/serde/de/trait.IntoDeserializer.html' title='serde::de::IntoDeserializer'>IntoDeserializer</a>&lt;'de, <a class='enum' href='serde_value/enum.DeserializerError.html' title='serde_value::DeserializerError'>DeserializerError</a>&gt; for <a class='enum' href='serde_value/enum.Value.html' title='serde_value::Value'>Value</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- implementors/serde/ser/trait.Serialize.js
+++ implementors/serde/ser/trait.Serialize.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["serde_value"] = ["impl <a class='trait' href='https://docs.serde.rs/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='enum' href='serde_value/enum.Value.html' title='serde_value::Value'>Value</a>",];
+implementors["serde_value"] = ["impl <a class='trait' href='https://docs.rs/serde/1.0.1/serde/ser/trait.Serialize.html' title='serde::ser::Serialize'>Serialize</a> for <a class='enum' href='serde_value/enum.Value.html' title='serde_value::Value'>Value</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
